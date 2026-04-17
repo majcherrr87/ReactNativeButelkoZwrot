dfa283bd0ca4d13109b8c64a4e75e3519bc8e35c
Adrian Majcher <majcherrr87@gmail.com>
2026-04-17 22:52:14 +0200

    add deleted endpoint

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,33 @@ app.post("/api/transactions", async (req, res) => {
   }
 });
 
+app.delete("/api/transactions/:user_id", async (req, res) => {
+  try {
+    const { user_id } = req.params;
+
+    if (isNaN(parseInt(user_id))) {
+      return res.status(400).json({ message: "Invalid transaction ID" });
+    }
+
+    const result =
+      await sql`DELETE FROM transactions WHERE user_id = ${user_id} RETURNING *`;
+    console.log(result);
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Transaction not found" });
+    }
+
+    res.status(200).json({ message: "Transaction deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting transaction:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
   });
 });
+
+//52:45
