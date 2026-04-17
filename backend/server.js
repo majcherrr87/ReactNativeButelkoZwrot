dad74c305ac629d42023aa8a4cfbcd01eea89875
Adrian Majcher <majcherrr87@gmail.com>
2026-04-17 17:55:10 +0200

    add db

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+    id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+    console.log("Table created successfully");
+  } catch (error) {
+    console.log("Error creating table:", error);
+    process.exit(1); // status code 1 means failure, 0 means success
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("It`s working!");
 });
 
-console.log("my port is: ", process.env.PORT);
-
-app.listen(5001, () => {
-  console.log("Server is running on port 5001");
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
 });
