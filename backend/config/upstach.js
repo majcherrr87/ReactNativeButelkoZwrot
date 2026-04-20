5e440f523048fdb931849628f42f3160ee7537f9
Adrian Majcher <majcherrr87@gmail.com>
2026-04-20 18:12:45 +0200

    add ratelimiter

--- backend/config/upstach.js
+++ backend/config/upstach.js
@@ -2,8 +2,8 @@ import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
 import "dotenv/config";
 
-const ratelimiter = new Ratelimit({
+const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
-export default ratelimiter;
+export default ratelimit;
