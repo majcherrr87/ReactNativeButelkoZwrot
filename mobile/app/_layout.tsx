f22e0da47a9eb580cc27c20fce2dc8ad421dd973
Adrian Majcher <majcherrr87@gmail.com>
2026-04-24 17:11:18 +0200

    add clerk authentication

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,20 @@
-import { Stack } from "expo-router";
-import Safescreen from "../components/SafeScreen";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { Slot } from "expo-router";
+import SafeScreen from "../components/SafeScreen";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 export default function RootLayout() {
   return (
-    <Safescreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </Safescreen>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }
